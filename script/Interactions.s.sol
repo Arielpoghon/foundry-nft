16a7642f52ddbe2e3c85ec72d961cf9515c181dc
ArielPoghon <mnangatariel27@gmail.com>
2026-04-24 20:46:05 +0300

    Initial Foundry NFT project

---
+++ script/Interactions.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract MintBasicNft is Script {
+    string public constant TOKENURI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(TOKENURI);
+        vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    uint256 public constant TOKEN_ID = 0;
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+        flipMoodOnContract(mostRecentlyDeployed, TOKEN_ID);
+    }
+
+    function flipMoodOnContract(address contractAddress, uint256 tokenId) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).flipMood(tokenId);
+        vm.stopBroadcast();
+    }
+}
---
+++ test/integration/MoodNftIntegrationsTest.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {MoodNft} from "../../src/MoodNft.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
+
+contract MoodNftIntegrationsTest is Test {
+    DeployMoodNft public deployer;
+    MoodNft public moodNft;
+    address public USER = makeAddr("USER");
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+        moodNft = deployer.run();
+    }
+
+    function testViewTokenURI() public {
+        vm.prank(USER);
+        moodNft.mintNft();
+        console.log(moodNft.tokenURI(0));
+    }
+
+    function testFlipMoodIntegration() public {
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+        string memory sadSvgImageUri = deployer.svgToImageURI(sadSvg);
+        string memory expectedTokenUri = _expectedTokenUri(sadSvgImageUri);
+
+        vm.prank(USER);
+        moodNft.mintNft();
+
+        vm.prank(USER);
+        moodNft.flipMood(0);
+
+        string memory actualTokenUri = moodNft.tokenURI(0);
+        console.log(actualTokenUri);
+
+        assertEq(keccak256(abi.encodePacked(actualTokenUri)), keccak256(abi.encodePacked(expectedTokenUri)));
+    }
+
+    function _expectedTokenUri(string memory imageUri) internal pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "data:application/json;base64,",
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"Mood NFT", "description":"An NFT that reflects your mood!", "attributes":[{"trait_type":"Mood", "value":100}], "image":"',
+                            imageUri,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
+    }
+}
