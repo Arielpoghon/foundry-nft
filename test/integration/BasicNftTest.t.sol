9dd7ddecd95a4e3bb96ca10af66378cabdff397f
Ariel Poghon <mnangatariel27@gmail.com>
2026-04-30 04:32:31 -0400

    Remove unnecessary comment from BasicNftTest
    Removed commented line for clarity.

--- test/integration/BasicNftTest.t.sol
+++ test/integration/BasicNftTest.t.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-//netlify
-
 import {Test} from "forge-std/Test.sol";
 import {BasicNft} from "../../src/BasicNft.sol";
 import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
