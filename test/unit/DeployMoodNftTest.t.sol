80e9a315332e98d8b2b982f4dc44f0a98909b580
Ariel Poghon <mnangatariel27@gmail.com>
2026-05-10 22:25:29 +0300

    Update DeployMoodNftTest.t.sol

--- test/unit/DeployMoodNftTest.t.sol
+++ test/unit/DeployMoodNftTest.t.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-//remove this line
 import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
 import {Test} from "forge-std/Test.sol";
 
