35d8bdfc1813135e2afb93ecac6ed630e1d90385
Ariel Poghon <mnangatariel27@gmail.com>
2026-05-10 22:36:36 +0300

    Remove commented code from BasicNftTest

--- test/integration/BasicNftTest.t.sol
+++ test/integration/BasicNftTest.t.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-//my code
 import {Test} from "forge-std/Test.sol";
 import {BasicNft} from "../../src/BasicNft.sol";
 import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
-
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
